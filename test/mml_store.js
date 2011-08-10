3c2de81903611f1f15932edba09055d5378b0a73
Simon Tokumine <simon@japancentre.com>
2011-08-10 10:54:39 +0100

    mmlstore and builder framework

--- test/mml_store.js
+++ test/mml_store.js
@@ -1,11 +1,23 @@
-var fs = require('fs');
-var path = require('path');
-var assert = require('assert');
+var assert     = require('assert');
+var _          = require('underscore');
 var grainstore = require('../lib/grainstore');
-var tests = module.exports = {};
+var tests      = module.exports = {};
 
 
 tests['true'] = function() {
   assert.ok(true);
 }
+
+tests['can create new instance of mml_store'] = function() {
+  var mml_store = new grainstore.MMLStore();
+  assert.includes(_.functions(mml_store), 'mml_builder');
+}
+
+tests['cannot create new mml_builders with blank opts'] = function() {
+  var mml_store = new grainstore.MMLStore();
+  assert.throws(function(){ 
+    mml_store.mml_builder();
+  }, Error, "Options must include db_name and table_name");
+}
+
 
