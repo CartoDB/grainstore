ab057f3cfc2251b5f5f855d7c04062e6b2476e46
Simon Tokumine <simon@japancentre.com>
2011-12-08 23:02:53 +0000

    allow passing of dynamic style to XML builder object

--- test/mml_builder.js
+++ test/mml_builder.js
@@ -18,28 +18,28 @@ tests['can generate base mml with normal ops'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table'});
   var baseMML = mml_builder.baseMML();
-  
+
   assert.ok(_.isArray(baseMML.Layer));
   assert.equal(baseMML.Layer[0].id, 'my_table');
-  assert.equal(baseMML.Layer[0].Datasource.dbname, 'my_database');  
+  assert.equal(baseMML.Layer[0].Datasource.dbname, 'my_database');
 }
 
 tests['can generate base mml with sql ops, maintain id'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table', sql: 'SELECT * from my_table'});
   var baseMML = mml_builder.baseMML();
-  
+
   assert.equal(baseMML.Layer[0].id, 'my_table');
-  assert.equal(baseMML.Layer[0].Datasource.table, 'SELECT * from my_table');  
+  assert.equal(baseMML.Layer[0].Datasource.table, 'SELECT * from my_table');
 }
 
 tests['can force plain base mml with sql ops'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table', sql: 'SELECT * from my_table'});
   var baseMML = mml_builder.baseMML({use_sql: false});
-  
+
   assert.equal(baseMML.Layer[0].id, 'my_table');
-  assert.equal(baseMML.Layer[0].Datasource.table, 'my_table');  
+  assert.equal(baseMML.Layer[0].Datasource.table, 'my_table');
 }
 
 tests['can generate full mml with style'] = function() {
@@ -87,7 +87,7 @@ tests['store a good style'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table'});
   mml_builder.setStyle("#my_table {\n  background-color: #fff;\n}", function(err, output){});
-}
+};
 
 tests['store a good style and retrieve it'] = function() {
   var style = "#my_table {\n  background-color: #fff;\n}"
@@ -96,7 +96,7 @@ tests['store a good style and retrieve it'] = function() {
   mml_builder.setStyle(style, function(err, output){
     mml_builder.getStyle(function(err, data){
       assert.eql(data.style, style);
-    });    
+    });
   });
 };
 
@@ -116,22 +116,32 @@ tests['store a good style and delete it, resetting to default'] = function() {
 tests['retrieves a non-existant style should return default style'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_databaasez', table:'my_tablez'});
-  
+
   mml_builder.getStyle(function(err, data){
     assert.eql(data.style, "#my_tablez {marker-fill: #FF6600;marker-opacity: 1;marker-width: 8;marker-line-color: white;marker-line-width: 3;marker-line-opacity: 0.9;marker-placement: point;marker-type: ellipse;marker-allow-overlap: true;}");
-  });    
-}
+  });
+};
+
+tests['retrieves a dynamic style should return XML with dynamic style'] = function() {
+  var mml_store = new grainstore.MMLStore(redis_opts);
+  var mml_builder = mml_store.mml_builder({dbname: 'my_databaasez', table:'my_tablez', style: '#my_tablez {marker-fill: #000000;}'});
+  mml_builder.toXML(function(err, data){
+    var xmlDoc = libxmljs.parseXmlString(data);
+    var color = xmlDoc.get("//@fill");
+    assert.eql(color.text(), "#000000");
+  });
+};
 
 tests['can retrieve basic XML'] = function() {
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_databaasez', table:'my_tablez'});
-  
+
   mml_builder.toXML(function(err, data){
     var xmlDoc = libxmljs.parseXmlString(data);
     var sql = xmlDoc.get("//Parameter[@name='table']");
     assert.eql(sql.text(), "my_tablez");
-  });    
-}
+  });
+};
 
 tests["can retrieve basic XML specifying sql"] = function(){
   var mml_store = new grainstore.MMLStore(redis_opts);
@@ -147,15 +157,15 @@ tests["can retrieve basic XML specifying sql"] = function(){
 tests["can retrieve basic XML specifying polygon default geom"] = function(){
   var mml_store = new grainstore.MMLStore(redis_opts);
   var mml_builder = mml_store.mml_builder({dbname: 'my_databaasez', table: 'my_polygon_tablez', geom_type: 'polygon'});
-  
+
   mml_builder.toXML(function(err, data){
     var xmlDoc = libxmljs.parseXmlString(data);
     var sql = xmlDoc.get("//Parameter[@name='table']");
     assert.eql(sql.text(), "my_polygon_tablez");
-    
+
     var style = xmlDoc.get("//PolygonSymbolizer");
-    assert.eql(style.attr('fill').value(), "#ff6600");    
-  });      
+    assert.eql(style.attr('fill').value(), "#ff6600");
+  });
 }
 
 tests["can set style and then retrieve XML"] = function(){
@@ -183,7 +193,7 @@ tests["can set style and then retrieve XML specifying sql"] = function(){
       var style = xmlDoc.get("//MapSymbolizer");
       assert.eql(style.attr('background-color').value(), "#000000");
     });
-  });  
+  });
 }
 
 tests["can set style and then retrieve XML specifying sql, then update style and regenerate"] = function(){
@@ -196,15 +206,16 @@ tests["can set style and then retrieve XML specifying sql, then update style and
       var xmlDoc = libxmljs.parseXmlString(data);
       var style = xmlDoc.get("//MapSymbolizer");
       assert.eql(style.attr('background-color').value(), "#000000");
-      
+
       var style2 = "#big_tester {\n  background-color: #999999;\n}"
       mml_builder.setStyle(style2, function(err, output){
         mml_builder.toXML(function(err, data){
           var xmlDoc = libxmljs.parseXmlString(data);
           var style = xmlDoc.get("//MapSymbolizer");
           assert.eql(style.attr('background-color').value(), "#999999");
-        });        
-      });      
+        });
+      });
     });
-  });  
+  });
 };
+
