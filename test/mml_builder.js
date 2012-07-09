5b94e8e7a1a019cd846b2fadc051a5854219eb54
Sandro Santilli <strk@keybit.net>
2012-07-09 10:57:27 +0200

    Add test for overridden authentication
    Test driven learning, for
    https://github.com/Vizzuality/Windshaft-cartodb/issues/18

--- test/mml_builder.js
+++ test/mml_builder.js
@@ -22,6 +22,27 @@ suite('mml_builder', function() {
     assert.equal(baseMML.Layer[0].Datasource.dbname, 'my_database');
   });
 
+  test('can generate base mml with overridden authentication', function() {
+    var mml_store = new grainstore.MMLStore(redis_opts, {
+        datasource: {
+            user:'overridden_user',
+            password:'overridden_password'
+        }});
+    var mml_builder = mml_store.mml_builder({
+            dbname: 'my_database',
+            table:'my_table',
+            // NOTE: authentication tokens here are silengly discarded
+            user:'shadow_user', password:'shadow_password'
+        });
+    var baseMML = mml_builder.baseMML();
+
+    assert.ok(_.isArray(baseMML.Layer));
+    assert.equal(baseMML.Layer[0].id, 'my_table');
+    assert.equal(baseMML.Layer[0].Datasource.dbname, 'my_database');
+    assert.equal(baseMML.Layer[0].Datasource.user, 'overridden_user');
+    assert.equal(baseMML.Layer[0].Datasource.password, 'overridden_password');
+  });
+
   test('can generate base mml with sql ops, maintain id', function() {
     var mml_store = new grainstore.MMLStore(redis_opts);
     var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table', sql: 'SELECT * from my_table'});
