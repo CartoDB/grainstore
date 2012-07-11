b0e96030e3bde22eb7ab094a0f28bf2bbed83a11
Sandro Santilli <strk@keybit.net>
2012-07-11 15:51:15 +0200

    Add more post-test cleanups

--- test/mml_store.js
+++ test/mml_store.js
@@ -19,14 +19,18 @@ test('cannot create new mml_builders with blank opts', function() {
   }, Error, "Options must include dbname and table");
 });
 
-test('can create new mml_builders with normal ops', function() {
+test('can create new mml_builders with normal ops', function(done) {
   var mml_store = new grainstore.MMLStore(redis_opts);
-  var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table'});
+  var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table'}, function() {
+    mml_builder.delStyle(done);
+  });
 });
 
-test('can create new mml_builders with normal ops and sql', function() {
+test('can create new mml_builders with normal ops and sql', function(done) {
   var mml_store = new grainstore.MMLStore(redis_opts);
-  var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table', sql: "select * from whatever"});
+  var mml_builder = mml_store.mml_builder({dbname: 'my_database', table:'my_table', sql: "select * from whatever"}, function(err, payload) {
+    mml_builder.delStyle(done);
+  });
 });
 
 });
